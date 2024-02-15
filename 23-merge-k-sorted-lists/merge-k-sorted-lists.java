4918d03c37fd77ccf9bad16bf07b7a01d5d8d11e
Sneha-2003 <107360158+Sneha-2003@users.noreply.github.com>
2024-02-16 00:49:49 +0530

    Time: 4 ms (70.59%) | Memory: 44.7 MB (18.14%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -10,30 +10,27 @@
  */
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> {
-            return a.val - b.val;
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->{
+            return a.val-b.val;
         });
-
-        ListNode dummy = new ListNode(-1);
-
-        for (ListNode l : lists) {
-            if (l != null) {
-                pq.add(l);
-            }
+        for(ListNode l:lists)
+        {
+            if(l!=null)
+            pq.add(l);
         }
-        
-        ListNode prev = dummy;
+        ListNode dummy=new ListNode(-1);
+        ListNode prev=dummy;
+        while(pq.size()!=0)
+        {
+           ListNode node=pq.remove();
+           prev.next=node;
+           prev=prev.next;
+           //node=node.next;
 
-        while (pq.size() != 0) {
-
-            // remove the node
-            ListNode node = pq.remove();
-            prev.next = node;
-            prev = prev.next;
-            node = node.next;
-
-            if (node != null)
-                pq.add(node);
+           if(node.next!=null)
+           {
+               pq.add(node.next);
+           }
         }
 
         return dummy.next;
