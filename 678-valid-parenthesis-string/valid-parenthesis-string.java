eeb6fdbe15e9ac9e0c83b5f8d334562cb460527f
Sneha-2003 <107360158+Sneha-2003@users.noreply.github.com>
2024-02-18 15:59:28 +0530

    Time: 1 ms (44.50%) | Memory: 41.3 MB (48.67%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,36 +1,38 @@
 class Solution {
-    public boolean checkValidString(String s) { 
-        Stack<Integer> b=new Stack<>();
+    public boolean checkValidString(String s) {
+        Stack<Integer> left_par=new Stack<>();
         Stack<Integer> star=new Stack<>();
 
         for(int i=0;i<s.length();i++)
-        {
-             if(s.charAt(i)=='(')
-             b.add(i);
-             else if(s.charAt(i)=='*')
-             star.add(i);
-             else
-             {
-                 if(b.size()>0)
-             b.pop();
-             else if(star.size()>0)
-             star.pop();
-             else 
-             return false;
-             }
+        {  char ch=s.charAt(i);
+            if(ch=='(')
+            left_par.add(i);
+            else if(ch=='*')
+            star.add(i);
+            else
+            {
+                if(left_par.size()>0)
+                left_par.pop();
+                else if(star.size()>0)
+                star.pop();
+                else
+                return false;
+            }
         }
-       while(b.size()>0)
-       {
-           if(star.size()>0)
-           {
-                        if(  star.pop() < b.pop())
-                        return false;
-           }
-           else
-           return false;
-       }
-      
-      return true;
-        
+
+        while(left_par.size()>0)
+        {
+            if(star.size()>0)
+            {
+                 if(star.pop()< left_par.pop())
+                 return false;
+            }
+            else
+            return false;
+        }
+
+        return true;
+
+
     }
 }
